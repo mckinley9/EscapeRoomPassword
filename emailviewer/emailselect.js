30fa670981fd33b64d9de1396380c0aa315f1e07
mckinley9 <mckinleylaberge@gmail.com>
2024-06-25 17:00:11 -0500

    Delete emailviewer/emailselect.js

--- emailviewer/emailselect.js
+++
@@ -1,8 +0,0 @@
-function showEmail(emailId) {
-    const emails = document.querySelectorAll('.email');
-    emails.forEach(email => {
-        email.style.display = 'none';
-    });
-
-    document.getElementById(emailId).style.display = 'block';
-}
