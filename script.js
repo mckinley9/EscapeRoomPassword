dab302cf11c2caaa40d5e094511ae5f635a8759c
mckinley9 <mckinleylaberge@gmail.com>
2024-06-25 16:33:27 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function checkPassword() {
-    const password = document.getElementById('password').value;
+    const password = document.getElementById('body').value.trim();
     const correctPassword = 'yourpassword';  // Replace with your actual password
 
     if (password === correctPassword) {
-        document.getElementById('password-section').style.display = 'none';
+        document.getElementById('email-draft-section').style.display = 'none';
         document.getElementById('protected-content').style.display = 'block';
     } else {
         alert('Incorrect password. Please try again.');
